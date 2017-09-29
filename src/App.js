fd851fce53a49ac6d7b6da2e90fb53b03622f3e7
Michael Yang <ledongyang@gmail.com>
2017-09-29 17:17:58 -0400

    routes home page

--- src/App.js
+++ src/App.js
@@ -27,13 +27,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
         <form onSubmit={this.addMessage.bind(this)}>
           <input type="text" ref={ el => this.inputEl = el }/>
           <input type="submit"/>
