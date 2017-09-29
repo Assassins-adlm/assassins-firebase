118e7881d6a16557e86c9f213eda2da5b0440b2b
Michael Yang <ledongyang@gmail.com>
2017-09-29 15:05:22 -0400

    firebase setup

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+  apiKey: "AIzaSyAFYb47n-YcDhRxHivbFM9f66VT5p6X46g",
+  authDomain: "assassins-aldm.firebaseapp.com",
+  databaseURL: "https://assassins-aldm.firebaseio.com",
+  projectId: "assassins-aldm",
+  storageBucket: "assassins-aldm.appspot.com",
+  messagingSenderId: "510113560850"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
