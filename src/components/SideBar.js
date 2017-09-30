71c0692b82b1fa893dd3c5c5e833137a7c177bb5
Michael Yang <ledongyang@gmail.com>
2017-09-30 11:53:44 -0400

    fix warning

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -18,15 +18,15 @@ class SideBar extends React.Component {
         <img style={avatarStyle} src="https://apollo2.dl.playstation.net/cdn/EP1563/CUSA04811_00/s43pXGobw83imLJSPmyutqWRjbU11jcD.png" alt="avatar"/>
         <span>Nameless</span></Link>
         <hr/>
-        <Link id="targets" className="menu-item" to="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span>Find Targets</span></Link>
-        <Link id="chat" className="menu-item" to="#"><i class="fa fa-comments" aria-hidden="true"></i><span>Chat</span></Link>
-        <Link id="lists" className="menu-item" to="#"><i class="fa fa-list" aria-hidden="true"></i><span>Target Lists</span></Link>
-        <Link id="about" className="menu-item" to="#"><i class="fa fa-info-circle" aria-hidden="true"></i><span>About</span></Link>
+        <Link id="targets" className="menu-item" to="#"><i className="fa fa-map-marker" aria-hidden="true"></i><span>Find Targets</span></Link>
+        <Link id="chat" className="menu-item" to="#"><i className="fa fa-comments" aria-hidden="true"></i><span>Chat</span></Link>
+        <Link id="lists" className="menu-item" to="#"><i className="fa fa-list" aria-hidden="true"></i><span>Target Lists</span></Link>
+        <Link id="about" className="menu-item" to="#"><i className="fa fa-info-circle" aria-hidden="true"></i><span>About</span></Link>
         <hr/>
-        <Link id="shop" className="menu-item" to="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Shop</span></Link>
+        <Link id="shop" className="menu-item" to="#"><i className="fa fa-shopping-cart" aria-hidden="true"></i><span>Shop</span></Link>
         <hr/>
-        <Link onClick={ this.showSettings } className="menu-item--small" to="#"><i class="fa fa-cog" aria-hidden="true"></i><span>Settings</span></Link>
-        <Link id="logout" className="menu-item" to="#"><i class="fa fa-sign-out" aria-hidden="true"></i><span>Logout</span></Link>
+        <Link onClick={ this.showSettings } className="menu-item--small" to="#"><i className="fa fa-cog" aria-hidden="true"></i><span>Settings</span></Link>
+        <Link id="logout" className="menu-item" to="#"><i className="fa fa-sign-out" aria-hidden="true"></i><span>Logout</span></Link>
       </Menu>
     );
   }
