a931f23b0c3b986b5b56b6bdd053222f13a9c78a
Michael Yang <ledongyang@gmail.com>
2017-09-30 12:00:08 -0400

    dont need webpack, use react-scripts start for development

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,6 @@ import Home from './Home';
 import SideBar from './SideBar';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-
 export default class Main extends React.Component {
 
   constructor(props) {
