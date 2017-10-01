6d1c70b9cb2b305a4898d012c69adda144041f66
Anton Landauer <anton.landauer@gmail.com>
2017-10-01 15:50:33 -0400

    store setup complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
-import fire from  '../fire.js';
 import FirebaseUIAuth from './firebaseUIAuth';
 import firebase, { ui } from '../fire'
 import SideBar from './SideBar';
 import '../index.css';
-import {connect} from 'react-redux'
-import {currentPlayer} from '../store/index'
+import { connect } from 'react-redux'
+import { currentPlayer } from '../store/index'
 
-
-const mapStateToProps = function ( state ) {
-	return {
-		player: state.player
-	}
-}
 class App extends Component {
 	state = {
 		loading: true,
-		user: null
+		user   : null
 	}
 
 	constructor() {
 		super();
 		this.uiConfig = {
 			// Called when the user has been successfully signed in.
-			callbacks: {
-				signInSuccess: (currentUser, credential, redirectUrl) => {
+			callbacks    : {
+				signInSuccess: ( currentUser, credential, redirectUrl ) => {
 					// Do not redirect.
 					return false;
 				}
 			},
 			// Opens IDP Providers sign-in flow in a popup.
-			signInFlow: 'popup',
+			signInFlow   : 'popup',
 			signInOptions: [
 				{
 					provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-					scopes: ['https://www.googleapis.com/auth/plus.login']
+					scopes  : ['https://www.googleapis.com/auth/plus.login']
 				},
 				{
 					provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-					scopes: [
+					scopes  : [
 						'public_profile',
 						'email',
 						'user_likes',
@@ -48,23 +41,26 @@ class App extends Component {
 				firebase.auth.TwitterAuthProvider.PROVIDER_ID,
 				firebase.auth.GithubAuthProvider.PROVIDER_ID,
 				{
-					provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+					provider          : firebase.auth.EmailAuthProvider.PROVIDER_ID,
 					// Whether the display name should be displayed in Sign Up page.
 					requireDisplayName: true
 				}
 			],
 			// Terms of service url.
-			tosUrl: 'https://www.google.com'
+			tosUrl       : 'https://www.google.com'
 		};
+		this.getPlayer = this.getPlayer.bind(this)
 	}
 
 	componentDidMount() {
-		firebase.auth().onAuthStateChanged((user) => {
-			this.setState({loading: false, user});
+		firebase.auth().onAuthStateChanged(( user ) => {
+			this.setState({ loading: false, user });
 		});
+		this.state.user ? this.props.getPlayer(this.state.user.email) : null
 	}
+
 	deleteAccount = () => {
-		firebase.auth().currentUser.delete().catch((error) => {
+		firebase.auth().currentUser.delete().catch(( error ) => {
 			if (error.code === 'auth/requires-recent-login') {
 				// The user's credential is too old. She needs to sign in again.
 				firebase.auth().signOut().then(() => {
@@ -77,52 +73,56 @@ class App extends Component {
 			}
 		});
 	}
+	getPlayer = ( data ) => {
+		this.props.getPlayer(data)
+	}
 
 	render() {
-		this.props.getPlayer('HI THERE') // Store TEST
 		return (
-            <div className="App">
+			<div className="App">
 				{this.state.loading ? (
-                    <div id="loading">Loading...</div>
+					<div id="loading">Loading...</div>
 				) : (
 					this.state.user ? (
-                        <div>
-													<SideBar />
-													<div>Home page!!
-														<div id="user-info">
-															<div id="photo-container">
-																<img id="photo" src={this.state.user.photoURL} alt={this.state.user.displayName} />
-															</div>
-															<div>{this.state.user.displayName}</div>
-															<div>{this.state.user.email}</div>
-														</div>
-														<p>
-															<button onClick={() => {firebase.auth().signOut()}}>Sign Out</button>
-															<button onClick={this.deleteAccount}>Delete account</button>
-														</p>
-													</div>
-                        </div>
+						<div>
+							<SideBar/>
+							<div>Home page!!
+								<div id="user-info">
+									<div id="photo-container">
+										<img id="photo" src={this.state.user.photoURL}
+										     alt={this.state.user.displayName}/>
+									</div>
+									<div>{this.state.user.displayName}</div>
+									<div>{this.state.user.email}</div>
+								</div>
+								<p>
+									<button onClick={() => {firebase.auth().signOut()}}>Sign Out</button>
+									<button onClick={this.deleteAccount}>Delete account</button>
+								</p>
+							</div>
+						</div>
 					) : (
-                        <div>
-                          <h4>You are signed out.</h4>
-                          <FirebaseUIAuth ui={ui} {...this.uiConfig} />
-                        </div>
+						<div>
+							<h4>You are signed out.</h4>
+							<FirebaseUIAuth ui={ui} {...this.uiConfig} />
+						</div>
 					)
 				)}
-            </div>
+			</div>
 		);
 	}
 }
 
 const mapDispatchToProps = ( dispatch ) => {
-	return	{
-		getPlayer(evt) {
+	return {
+		getPlayer( evt ) {
+			console.log(evt)
 			dispatch(currentPlayer(evt))
 		}
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(state => state, mapDispatchToProps)(App)
 
 
 
