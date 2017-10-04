07734079fe0615f27736ebf0ef544b7bb8896bf2
Michael Yang <ledongyang@gmail.com>
2017-10-04 01:03:41 -0400

    info windows

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,54 +1,55 @@
-import fetch from "isomorphic-fetch";
 import firebase from '../fire'
 import React from 'react'
-import { compose, withProps } from "recompose";
+// import FaAnchor from 'react-icons/lib/fa/ancho'
 import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
-import {
-	firebaseConnect,
-	isLoaded,
-	isEmpty,
-	dataToJS,
-	populateDataToJS,
-	pathToJS
-} from 'react-redux-firebase'
+	withScriptjs,
+	withGoogleMap,
+	GoogleMap,
+	Marker,
+	InfoWindow
+} from 'react-google-maps'
+// import {
+// 	firebaseConnect,
+// 	isLoaded,
+// 	isEmpty,
+// 	dataToJS,
+// 	populateDataToJS,
+// 	pathToJS
+// } from 'react-redux-firebase'
+import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer'
 
+const MyMarker = () => {
+	return (
+		<h1>wow</h1>
+	)
+}
 
-
-import MarkerClusterer from "react-google-maps/lib/components/addons/MarkerClusterer";
-
-const MapWithAMarkerClusterer = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: '100vh' }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props =>
-  <GoogleMap
-    defaultZoom={15}
-    defaultCenter={{ lat: 40.704, lng: -74.009}}
-  >
-    <MarkerClusterer
-      averageCenter
-      enableRetinaIcons
-      gridSize={60}
-    >
-      {props.markers.map(marker => (
-        <Marker
-          key={marker.photo_id}
-          position={{ lat: marker[0], lng: marker[1] }}
-        />
-      ))}
-    </MarkerClusterer>
-  </GoogleMap>
-);
+const MapWithAMarkerClusterer = withScriptjs(withGoogleMap(props =>
+	<GoogleMap
+		defaultZoom={15}
+		defaultCenter={{ lat: 40.704, lng: -74.009}}
+	>
+		<MarkerClusterer
+			averageCenter
+			enableRetinaIcons
+			gridSize={60}
+		>
+			{props.markers.map((marker, idx) => (
+				<Marker
+					key={idx}
+					position={{ lat: marker[0], lng: marker[1] }}
+					onClick={props.onToggleOpen}
+				>
+					{
+						props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+							<MyMarker />
+						</InfoWindow>
+					}
+				</Marker>
+			))}
+		</MarkerClusterer>
+	</GoogleMap>
+))
 
 export default class MapBox extends React.PureComponent {
 
@@ -56,36 +57,48 @@ export default class MapBox extends React.PureComponent {
 		super()
 		this.state={
 			players:[],
-	  	markers: []
+			markers: [],
+			isOpen: false
 		}
+		this.onToggleOpen = this.onToggleOpen.bind(this)
 	}
 
-  componentWillMount() {
+	onToggleOpen() {
+		console.log('toggle')
+		this.setState({isOpen: !this.state.isOpen})
+	}
+
+	componentWillMount() {
 		const playersRef = firebase.database().ref('players')
-			playersRef.on('value', (snapshot) => {
-				let players = snapshot.val()
-				let newlocations = []
-				for(var player in players){
-					console.log(players[player].location, "for loop")
-					newlocations.push(players[player].location)
-
-				}
-				console.log(newlocations, "!!!!")
-				this.setState({
-					markers: newlocations
-				})
+		playersRef.on('value', (snapshot) => {
+			let players = snapshot.val()
+			let newlocations = []
+			for(var player in players){
+				console.log(players[player].location, 'for loop')
+				newlocations.push(players[player].location)
+			}
+			console.log(newlocations, '!!!!')
+			this.setState({
+				markers: newlocations
 			})
-  }
+		})
+	}
 
-
-  render() {
-
+	render() {
 		let user = this.state.players
-
- //markers={this.state.markers}
-    return (
-      <MapWithAMarkerClusterer markers={this.state.markers}/>
-    )
-  }
+		//markers={this.state.markers}
+		return (
+			<MapWithAMarkerClusterer
+				googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+				loadingElement={<div style={{ height: '100%' }} />}
+				containerElement={<div style={{ height: '100vh' }} />}
+				mapElement={<div style={{ height: '100%' }} />}
+				markers={this.state.markers}
+				onToggleOpen={this.onToggleOpen}
+				isOpen={this.state.isOpen}
+			/>
+		)
+	}
 }
+
 
