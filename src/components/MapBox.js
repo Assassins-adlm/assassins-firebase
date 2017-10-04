dd7d988150684ce43f648413b38a38702c697b19
Denis Gao <denisgao@buffalo.edu>
2017-10-04 12:48:03 -0400

    google maps theme

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,3 +1,4 @@
+
 import fetch from "isomorphic-fetch";
 import firebase from '../fire'
 import React from 'react'
@@ -34,6 +35,7 @@ const MapWithAMarkerClusterer = compose(
   <GoogleMap
     defaultZoom={15}
     defaultCenter={{ lat: 40.704, lng: -74.009}}
+    defaultOptions = {{ styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]}}
   >
     <MarkerClusterer
       averageCenter
@@ -78,9 +80,12 @@ export default class MapBox extends React.PureComponent {
   }
 
 
+
   render() {
 
 		let user = this.state.players
+
+		console.log("user", user)
 
  //markers={this.state.markers}
     return (
