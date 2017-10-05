0b258b54ba7586d66244c19295fcd369ac83f49e
Michael Yang <ledongyang@gmail.com>
2017-10-05 11:52:32 -0400

    fix zoom in when render direction

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -2,6 +2,7 @@
 import firebase from '../fire'
 import React from 'react'
 import MyTarget from './TargetInfo'
+import MyInfo from './MyInfo'
 import {
 	withGoogleMap,
 	GoogleMap,
@@ -22,16 +23,17 @@ import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClust
 const MapWithAMarkerClusterer = withGoogleMap(props =>{
 	// console.log('props-->', props)
 	let myLocation = props.currPlayer.location
+	let targetLocation = props.currTarget.location
 	return (
 		<GoogleMap
-			defaultZoom={15}
+			zoom={15}
 			defaultCenter={{ lat: myLocation[0], lng: myLocation[1]}}
-			defaultOptions={{ styles: props.mapStyles }}
+			options={{ styles: props.mapStyles, mapTypeControl: false }}
 		>
-			<Circle defaultCenter={{ lat: myLocation[0], lng: myLocation[1]}}
-				defaultRadius={500}
+			<Circle center={{ lat: targetLocation[0], lng: targetLocation[1]}}
+				radius={1000}
 			/>
-			{props.directions && <DirectionsRenderer defaultOptions={{suppressMarkers: true}} 				directions={props.directions}
+			{props.directions && <DirectionsRenderer options={{preserveViewport: true, suppressMarkers: true}} directions={props.directions}
 			/>}
 			<MarkerClusterer
 				averageCenter
@@ -52,7 +54,9 @@ const MapWithAMarkerClusterer = withGoogleMap(props =>{
 						>
 							{
 								marker.openInfo && <InfoWindow onCloseClick={() => {props.onToggleOpen(marker)}}>
-									<MyTarget target={marker}/>
+									{
+										marker.id === props.currPlayer.id ? <MyInfo currPlayer={props.currPlayer}/> : <MyTarget target={marker}/>
+									}
 								</InfoWindow>
 							}
 						</Marker>
@@ -187,6 +191,7 @@ class MapBox extends React.PureComponent {
 				markers={this.state.markers}
 				onToggleOpen={this.onToggleOpen}
 				currPlayer={this.state.currPlayer}
+				currTarget={this.state.currTarget}
 				directions={this.state.directions}
 				mapStyles={[{'featureType':'all','elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'color':'#000000'},{'lightness':13}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#144b53'},{'lightness':14},{'weight':1.4}]},{'featureType':'landscape','elementType':'all','stylers':[{'color':'#08304b'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#0c4152'},{'lightness':5}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#0b434f'},{'lightness':25}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'color':'#0b3d51'},{'lightness':16}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'}]},{'featureType':'transit','elementType':'all','stylers':[{'color':'#146474'}]},{'featureType':'water','elementType':'all','stylers':[{'color':'#021019'}]}]}
 			/>
