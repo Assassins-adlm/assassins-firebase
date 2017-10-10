521da3df4a5eac79a99ee66eb9a1e790594d8630
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2017-10-09 20:33:39 -0400

    fetching message from reducer

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -2,33 +2,16 @@ import React from 'react'
 import firebase from '../fire'
 import {Link} from 'react-router-dom'
 import '../chatRoom.css'
+// testing fetchAllMessages
+import {fetchAllMessages} from '../store'
+import { compose } from 'redux'
+import {firebaseConnect, dataToJS, pathToJS, isLoaded} from 'react-redux-firebase'
+import {connect} from 'react-redux'
 
-export default class ChatRoom extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { messages: [] } // <- set up react state
-	}
+class ChatRoom extends React.Component {
 
 	componentDidMount() {
-		let messagesRef = firebase.database().ref('messages').limitToLast(100)
-		// messagesRef.once('value').then(snapshot =>{
-		// 	const message = Object.values(snapshot.val())
-		// 	message.forEach(x => console.log(x.name, x.body))
-		// })
-
-		messagesRef.on('child_added', snapshot => {
-			let message = {
-				text: {
-					name: snapshot.val().name,
-					body: snapshot.val().body
-				},
-				id: snapshot.key
-			}
-			this.setState({ messages: this.state.messages.concat(message) })
-		})
-		firebase.auth().onAuthStateChanged((user) => {
-			this.setState({loading: false, user})
-		})
+		this.props.fetchAllMessages()
 	}
 
 	addMessage(e) {
@@ -44,32 +27,80 @@ export default class ChatRoom extends React.Component {
 	}
 
 	render() {
-		console.log('Local messages ', this.state.messages)
-
 		return (
-			this.state.user ?
-				<div id="wrapper" className="row">
-					<div className="col-xs-12">
-						<div id="menu">
-							<p className="welcome">Welcome, <b>{this.state.user.displayName}</b></p>
-							<p className="logout"><Link id="exit" to="/home">Exit Chat</Link></p>
-							<div style={{clear:'both'}}></div>
-						</div>
-
-						<div id="chatbox">
-							<ul style={{listStyle: 'none'}}>
-								{
-									this.state.messages.map( message => <li key={message.id}>{message.text.name}: {message.text.body}</li> )
-								}
-							</ul>
-						</div>
-
-						<form name="message" onSubmit={this.addMessage.bind(this)}>
-							<input name="usermsg" type="text" id="usermsg" size="63" ref={ el => this.inputEl = el }/>
-							<input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-						</form>
+		
+			<div id="wrapper" className="row">
+				<div className="col-xs-12">
+					<div id="menu">
+						<p className="logout"><Link id="exit" to="/home">Exit Chat</Link></p>
+						<div style={{clear:'both'}}></div>
 					</div>
-				</div> : <Link to="/home">Please Login!</Link>
+
+					<div id="chatbox">
+						<ul style={{listStyle: 'none'}}>
+							{
+								Array.isArray(this.props.messages) && this.props.messages.map((message,ind) =>{
+									return (
+										<li key={ind}>{message.name}: {message.body}</li>
+									)
+								}) 
+							}
+						</ul>
+					</div>
+
+					<form name="message" onSubmit={this.addMessage.bind(this)}>
+						<input name="usermsg" type="text" id="usermsg" size="63" ref={ el => this.inputEl = el }/>
+						<input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+					</form>
+				</div>
+			</div> 
 		)
 	}
 }
+// {
+// 	this.state.messages.map( message => <li key={message.id}>{message.text.name}: {message.text.body}</li> )
+// }
+
+// : <Link to="/home">Please Login!</Link>
+
+const mapStateToProps = (state) => {
+	return {
+		messages: state.message,
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		fetchAllMessages() {
+			dispatch(fetchAllMessages())
+		}
+		
+	}
+}
+
+export default compose(firebaseConnect([{path: 'auth'}]), connect(mapStateToProps, mapDispatchToProps))(ChatRoom)
+
+// console.log('wtf do we have here, ', fetchAllMessages)
+		
+
+// let messagesRef = firebase.database().ref('messages').limitToLast(100)
+// messagesRef.once('value').then(snapshot =>{
+// 	const message = Object.values(snapshot.val())
+// 	message.forEach(x => console.log(x.name, x.body))
+// })
+// this.props.fetchAllMessages()
+		
+
+// messagesRef.on('child_added', snapshot => {
+// 	let message = {
+// 		text: {
+// 			name: snapshot.val().name,
+// 			body: snapshot.val().body
+// 		},
+// 		id: snapshot.key
+// 	}
+// 	this.setState({ messages: this.state.messages.concat(message) })
+// })
+// firebase.auth().onAuthStateChanged((user) => {
+// 	this.setState({loading: false, user})
+// })
--- src/store/message.js
+++ src/store/message.js
@@ -17,8 +17,9 @@ export function allMessages(message) {
 // THUNKS
 export const fetchAllMessages = () => dispatch => {
 	let messagesRef = firebase.database().ref('messages').limitToLast(100)
-	messagesRef.once('value').then(snapshot =>{
-		dispatch(allMessages(getMessages(snapshot.val())))
+	messagesRef.once('value').then(snapshot => {
+		const messages = Object.values(snapshot.val())
+		dispatch(allMessages(messages))
 	})
 }
 
@@ -28,66 +29,11 @@ export const fetchAllMessages = () => dispatch => {
 export default function (state = messageState, action) {
 	switch(action.type){
 	case ALL_MESSAGES:
-		return [...state].concat(action.message)
+		return [...messageState.messages].concat(action.message)
 	default: 
 		return state
 	}
             
 }
 
-// HELPER FUNCS
-const getMessages = messages =>{
-	return Object.values(messages).map(message => 
-	{
-		message.name,
-		message.body
-	}
-	)}
 
-
-
-// export default function (state = playerState, action) {
-// 	switch (action.type) {
-// 	case CURRENT_PLAYER:
-// 		return {
-// 			...state,
-// 			player: action.player
-// 		}
-// 	case ALL_PLAYERS:
-// 		return {
-// 			...state,
-// 			players: action.players.map(player => {
-// 				return {...player, openInfo: false}
-// 			})
-// 		}
-// 	case TOGGLE_SELECTED_PLAYER:
-// 		return {
-// 			...state,
-// 			players: state.players.map(player => player.id === action.player.id ? {...player, openInfo: !player.openInfo} : player)
-// 		}
-// 	case CURRENT_LOCATION:
-// 		return Object.assign({}, state, { location: {latitude: action.location.latitude, longitude: action.location.longitude}})
-// 	case CURRENT_TARGET:
-// 		return {
-// 			...state,
-// 			target: action.target
-// 		}
-// 	default:
-// 		return state
-// 	}
-
-
-// console.log(Object.values(snapshot.val()))
-// let message = {
-// 	text: {
-// 		name: snapshot.val().name,
-// 		body: snapshot.val().body
-// 	},
-// 	id: snapshot.key
-// }
-// let messagesRef = firebase.database().ref('messages').limitToLast(100)
-// messagesRef.once('value').then(snapshot =>{
-// 	const message = Object.values(snapshot.val())
-// 	message.forEach(x => console.log(x.name, x.body))
-// })
-// }
